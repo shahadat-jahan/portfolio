08bf8463eebef6935429b41340ed3d32ee00bfd3
shahadat-jahan <shahadat.jahan@gmail.com>
2026-03-27 03:55:28 +0600

    Support Vercel deployment setup

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+const isGitHubPages = process.env.GITHUB_ACTIONS === "true";
+
 export default defineConfig({
-  base: "/portfolio/",
+  base: isGitHubPages ? "/portfolio/" : "/",
   plugins: [react(), tailwindcss()],
 });
